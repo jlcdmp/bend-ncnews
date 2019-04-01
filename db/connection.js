f2999879bad759ffad51c997eef0f17763a1b3fc
jlcdmp <joeoliverc123@gmail.com>
2019-04-01 09:22:16 +0100

    minor changes

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,5 @@
-const knex = require('knex');
+const ENV = process.env.NODE_ENV || 'development';
 
-const dbConfig = require('../knexfile');
+const config = ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } : require('../knexfile')[ENV];
 
-const connection = knex(dbConfig);
-
-module.exports = connection;
+module.exports = require('knex')(config);
