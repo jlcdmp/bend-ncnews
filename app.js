91900ab381fa96709ccd38346932c0cbbb0bf57f
jlcdmp <joeoliverc123@gmail.com>
2019-04-01 13:58:24 +0100

    cors added for fend intergration

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
+const cors = require('cors')
+
 const apiRouter = require('./routes/api-router');
 const { handle400s, handle404s, handle422s } = require('./error-handling');
 
 
 app.use(bodyParser.json());
+
+app.use(cors())
+
+
 app.use('/api', apiRouter);
 
 app.use('/*', (req, res, next) => {
